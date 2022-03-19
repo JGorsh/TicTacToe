69a13ed40d9848d4cc05a6d2815e3857be5882bc
JGorsh <alexmilesjg@gmail.com>
2022-03-20 00:08:16 +0500

    - Добавлено сохранения результата в JSON

---
+++ src/main/java/com/alex/controller/Json.java
@@ -0,0 +1,13 @@
+package com.alex.controller;
+
+import com.alex.repository.SaveParse;
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+import java.io.IOException;
+
+public class Json {
+    public static void main(String[] args) throws IOException {
+        SaveParse saveParse = new SaveParse();
+        //saveParse.saveJSON();
+    }
+}
